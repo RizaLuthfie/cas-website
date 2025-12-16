1b384273dad43ec3c37c2d3fef53e89053dd37a9
RizaLuthfie <169018770+RizaLuthfie@users.noreply.github.com>
2025-12-16 13:44:12 +0800

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'github' ? '/cas-website/' : '/',
+
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+}))
